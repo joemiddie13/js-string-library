5df25129e9c3c70a5a9e56a095984c9aeca05385
joemiddie13 <joemiddie13@gmail.com>
2024-10-08 17:11:46 -0700

    Added continuous integration and rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'es',
+      },
+    ],
+    plugins: [
+      typescript(),
+      commonjs(),
+      resolve(),
+    ],
+  },
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
